f96b2eb2e315ca22e59bbbc5250f4e26b3f73c7c
theck17 <theck17@163.com>
2021-07-15 19:01:31 +0800

    02

--- Chapter_02_Representation_and_processing_of_information/2.61.c
+++ Chapter_02_Representation_and_processing_of_information/2.61.c
@@ -7,15 +7,33 @@
  *   DateTime      ：2021-07-14 00:44:22
  *   Description   ：
  */
+#include <stdio.h>
 
-int main(){
-  // A
-  !~x;;
-  // B
-  !x;
-  // C
-  !~(x | ~0xff);
-  // D
-  !((x >> ((sizeof(int)-1) << 3)) & 0xff);
-    return 0;
+int main(int argc, char const *argv[])
+{
+
+	int x, y;	/* y means 0 should be returned */
+	int sizeof_int = sizeof(int);
+	
+	/*condition A*/
+	x = ~0;
+	y = 0xFFFFFF00;
+	printf("%d\t%d\n", !(~x), !(~y));
+	
+	/*condition B*/
+	x = 0;
+	y = 0x000000FF;
+	printf("%d\t%d\n", !x, !y);
+	
+	/*condition C*/
+	x = 0x000000FF;
+	y = 0x0000000F;
+	printf("%d\t%d\n", !((x ^ 0xFF)<<((sizeof_int-1)<<3)), !((y ^ 0xFF)<<((sizeof_int-1)<<3)));
+
+	/*condition D*/
+	x = 0x00FFFFFF;
+	y = 0x0FFFFFFF;
+	printf("%d\t%d\n", !(x >> ((sizeof_int-1) << 3)), !(y >> ((sizeof_int-1) << 3)));
+	return 0;
 }
+
---
+++ Chapter_02_Representation_and_processing_of_information/2.63.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+unsigned srl(unsigned x, int k);
+int sra(int x, int k); 
+
+int main(int argc, char const *argv[])
+{
+	printf("%#.8x\n", srl(0x80000000, 8));
+	printf("%#.8x\n", sra(0x80000000, 8));
+	return 0;
+}
+
+unsigned srl(unsigned x, int k) 
+{
+	/* Perform shift arithmetically */
+	unsigned xsra = (int) x >> k;
+	/*思路是由k形成诸如0x00FFFFFF这样的掩码，与xsra进行与操作从而将高位置零*/
+	unsigned w = sizeof(int) << 3;
+	unsigned mask = ~(((1 << k)-1)<<(w-k)); 
+  	/*(1 << k)-1能够获得低位连续为1，高位为0的掩码，但是其不能达到全1，于是继续向左移w-k然后取反*/
+	return mask & xsra; 
+}
+
+int sra(int x, int k)
+{
+	/* Perform shift logically */
+	int xsrl = (unsigned) x >> k;
+	/*这个题目的关键点是判断符号位是否为1，通过test &= xsrl，test为零如果符号位为0，否则test不变（处于符号位位置*/
+	unsigned w = sizeof(int) << 3;
+	int test = 1 << (w-1-k);
+	test &= xsrl;
+	int mask = ~(test - 1);
+  	/*test为零时，~(test - 1)��全零，不会改变xsrl*/
+	return mask | xsrl;
+}
